aa4483408b943b0c503fbdb7f9cedbfea87492ca
antonioggeorgiev <antonio.g.georgiev1998@gmail.com>
2025-02-24 11:12:30 +0200

    added shadcn calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Calendar } from "~/components/ui/calendar";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
@@ -10,7 +11,9 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"></main>
+      <main className="flex min-h-screen">
+        <Calendar />
+      </main>
     </HydrateClient>
   );
 }
